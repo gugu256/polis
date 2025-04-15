17625e74fe323bc9f8858c49623f1a3e92d75d74
gustave.dufresne <gustave.dufresne@monlycee.net>
2025-04-15 18:03:54 +0200

    init commit

---
+++ polis.py
@@ -0,0 +1,32 @@
+# POLIS MANAGER
+
+"""
+ARMY
+Bigger army = higher odds of winning wars and successful colonization
+Can be changed by: training and recruiting in the army, hiring mercenaries, mobilizing all men
+
+INFLUENCE
+Bigger influence = more chances to get allies in wars, more chances to get potential trading partners,
+more chances to attract métèques.
+Can be changed by sending diplomats and founding colonies
+
+TREASURY
+Bigger treasury = uhhh self explanatory everything costs money lol.
+Changing taxes, making your population work in mines to produce drachmae, establishing trade routes
+
+HAPPINESS
+More happiness = less chances of revolts
+Can be changed (positevely) by : organizing games and theater contests, founding colonies (sad people go away),
+lowering taxes, winning wars
+
+TRADE PARTNERS
+More of them = more trade = more money and influence
+Can be changed by asking cities to trade with you (and indirectly founding colonies)
+
+COLONY
+Founding colonies increases your citizens' happiness, but requires money and giving part of your army
+Increases infuence, happiness, and the number of trade partners
+
+POPULATION
+More of it = more chances at winning wars, more taxes coming in, 
+"""
